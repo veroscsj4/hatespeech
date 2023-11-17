553d627bb642d9d5086dc28f3fb600ce1668cbf8
Laura Amaro <79660843+laumagg@users.noreply.github.com>
2023-11-17 13:58:23 +0100

    imported report models

--- apps/report/models.py
+++ apps/report/models.py
@@ -1,7 +1,53 @@
 from django.db import models
-from datetime import datetime
+
+class Platform(models.Model):
+    X_TWITTER = 'X'
+    FACEBOOK = 'FACEBOOK'
+    INSTAGRAM = 'INSTAGRAM'
+    REDDIT  = 'REDDIT'
+    Platforms = [(X_TWITTER, "X/Twitter"), 
+                 (FACEBOOK, 'Facebook'),
+                 (INSTAGRAM, "Instagrm"),
+                  (REDDIT, "Rddit")]
+    platfrom_name = models.CharField(max_length=255, unique=True, choices=Platforms)
+
+    #class Meta:
+      #  app_label = 'default_db'
+
+    def __str__(self) -> str:
+        return self.plattfrom_name
+
+class Label(models.Model):
+    label_name = models.CharField(max_length= 255)
+
+    #class Meta:
+      #  app_label = 'default_db'
+
+    def __str__(self) -> str:
+        return self.label_name
+    
+class ClassifierRespoonse(models.Model):
+    Label = models.ForeignKey(Label, on_delete=models.SET_NULL, null=True)
+    timestamp = models.DateTimeField().auto_created
+
+    #class Meta:
+        #app_label = 'default_db'
+    def __str__(self) -> str:
+        return f"{self.Label.label_name if self.Label else 'No Label'} at {self.timestamp}"
+        
+
 
 class Post(models.Model):
-  img = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-  description = models.TextField(blank=True)
-  date = models.DateTimeField(default=datetime.now, blank=True)
+    post_content = models.TextField()
+    post_link = models.CharField(max_length=1024, blank=True, null=True)
+    post_image = models.CharField(max_length=1024, blank=True, null=True)
+    user_prediction = models.CharField(max_length=255)
+    platform = models.ForeignKey(Platform, on_delete=models.SET_NULL, null=True)
+    classifier_response = models.ForeignKey(ClassifierRespoonse, on_delete=models.SET_NULL, null=True)
+    timestamp = models.DateTimeField().auto_created
+
+    #class Meta:
+       # app_label = 'default_db'
+
+    def __str__(self) -> str:
+        return f"Post on {self.platform.platfrom_name} at {self.timestamp}"

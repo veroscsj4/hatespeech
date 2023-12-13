5ba283610d808a8b9e35b2632aa4b3655bb43784
Anja Tokmak <a.tokmak@emperra.com>
2023-12-13 15:37:27 +0100

    Add & process classifier request

--- backend/apps/report/views.py
+++ backend/apps/report/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from django.http import FileResponse
@@ -5,64 +6,73 @@ from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import FileUploadParser, MultiPartParser, FormParser
 
 from .models import Post
-from .serializers import *
-
-import configparser
-from minio import Minio
-from minio.error import S3Error
-from django.shortcuts import render
-from PIL import Image
-import io
-import uuid
+from .serializers import PostSerializer
 from .utils import UploadImageToMinio, GetImageFromMinio
+
+import os
+import subprocess
+
 
 @api_view(['POST'])
 def post_report(request):
     serializer = PostSerializer(data=request.data)
+
     if serializer.is_valid():
         serializer.save()
         print('Report post')
         return Response(status=status.HTTP_201_CREATED)
-    
-    print('Report post not valid')
 
+    print('Report post not valid')
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
 def get_reports(request):
     data = Post.objects.all()
-
     serializer = PostSerializer(data, context={'request': request}, many=True)
-
     print('Report post')
     return Response(serializer.data)
 
 
-@api_view(['POST','GET'])
+@api_view(['POST', 'GET'])
 @parser_classes([MultiPartParser, FormParser])
 def report(request):
     if request.method == 'GET':
         print('GET request')
-        
-    if request.method == 'POST':
-        # Get image from request and upload to minIO
-        # id, to save in DB and recall image.
-        id = UploadImageToMinio(request.data.get('file'))
-        # Update data in request, for DB
-        request.data['post_image'] = id
-        ### FOR TESTING
-        request.data['post_content'] = 'Test'
-        request.data['user_prediction'] = 'A'
-        # Get image from Minio, using id in DB
-        obj = GetImageFromMinio(id)
-        ### END TESTING
-        #Serialize and save in DB
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            print('Report post')
-            return FileResponse(obj, content_type='image/*')
+    elif request.method == 'POST':
+        file = request.data.get('file')
+
+        if file and file != 'undefined':
+            # Handle image upload
+            id = UploadImageToMinio(file)
+            request.data['post_image'] = id
+            # Additional data for testing
+            request.data['post_content'] = 'Test'
+            request.data['user_prediction'] = 'A'
+            # Save in DB
+            serializer = PostSerializer(data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+                obj = GetImageFromMinio(id)
+                return FileResponse(obj, content_type='image/*', as_attachment=True)
+        else:
+            classifier_response = request.data.get('classifier_response', '')
+            if classifier_response:
+                script_path = os.path.abspath('../backend/classifier/hate_speech_service/svm.py')
+                script_command = f'python {script_path} "{classifier_response}"'
+
+                try:
+                    output = subprocess.check_output(script_command, shell=True, text=True)
+                    print(f"Script Output: {output}")
+
+                    # If script succeeds, save to DB
+                    serializer = PostSerializer(data=request.data)
+                    if serializer.is_valid():
+                        serializer.save()
+                    return Response(status=status.HTTP_201_CREATED)
+                except subprocess.CalledProcessError as e:
+                    print(f"Script failed with error: {e}")
             return Response(status=status.HTTP_201_CREATED)
-        
+
     return render(request, 'report/dummyReport.html')

0b4f3f718198f0e5ec852126f606ff510d2fe3a1
ahmedAbouismail <s0565952@htw-berlin.de>
2024-01-07 14:08:09 +0100

    Added validation method for post_link field

--- backend/apps/report/serializers.py
+++ backend/apps/report/serializers.py
@@ -6,6 +6,16 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['post_content', 'post_link', 'post_image', 'user_prediction', 'post_platform', 'classifier_response']
 
+    def validate_post_link(self, value):
+        patterns = {
+            'twitter': r'^https?://(www\.)?twitter\.com/.*',
+            'facebook': r'^https?://(www\.)?facebook\.com/.*',
+            'instagram': r'^https?://(www\.)?instagram\.com/.*',
+            'reddit': r'^https?://(www\.)?reddit\.com/.*'
+        }
+
+        if not any(re.match(pattern, value) for pattern in patterns.values()):
+            raise serializers.ValidationError("Link is not valid")
 
 
 class ClassifierResponseSerializer(serializers.ModelSerializer):
